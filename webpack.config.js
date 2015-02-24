84d591d701e548f5cd93f0ba58a8f3dbe2a90d59
Olav Frengstad <olav@fwt.no>
2015-02-24 15:40:07 +0100

    tabs always beat spaces

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    entry: "./src/app.js",
+	entry: "./src/app.js",
 	output: {
 		path: "./build",
 		publicPath: "/build/",
 		filename: "dist.js"
 	},
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" },
-				{ test: /\.html$/, loader: "html" },
-				{ test: /\.vue$/, loader: "vue" },
-				{ test: /\.json/, loader: "json" }
-        ]
-    }
+	module: {
+		loaders: [
+			{ test: /\.css$/, loader: "style!css" },
+			{ test: /\.html$/, loader: "html" },
+			{ test: /\.vue$/, loader: "vue" },
+			{ test: /\.json/, loader: "json" }
+		]
+	}
 };
