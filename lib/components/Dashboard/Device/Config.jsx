13fcab9938ddafe3f4bb02d96c0b8a5fd13584f3
Olav Frengstad <olav@fwt.no>
2016-01-28 20:16:47 +0000

    Add config page shell

---
+++ lib/components/Dashboard/Device/Config.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Mixin from 'react-mixin'
+
+export class Config extends React.Component {
+  render() {
+    return (
+      <div>It takes two to configure!</div>
+    )
+  }
+}
